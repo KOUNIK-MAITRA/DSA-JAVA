cf35370522f2380aae71e2fdb3e0b7b2afa92a13
KOUNIK MAITRA <kounikmaitra2633@gmail.com>
2022-06-15 14:40:09 +0530

    Update LongestCommonSubsequence_TopDown.java

--- Dynamic-Programing/LongestCommonSubsequence_TopDown.java
+++ Dynamic-Programing/LongestCommonSubsequence_TopDown.java
@@ -1,33 +1,39 @@
-
-/**
- * Write a description of class LongestCommonSubsequence_TopDown here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class LongestCommonSubsequence_TopDown
+import java.io.*;
+import java.util.*;
+class LongestCommonSubsequence_TopDown
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class LongestCommonSubsequence_TopDown
-     */
-    public LongestCommonSubsequence_TopDown()
+    public static void main (String args[])
     {
-        // initialise instance variables
-        x = 0;
+        String a="ABCAB"; 
+        String b="AECB";
+        int m=a.length();
+        int n=b.length();
+        System.out.println("length of the longest subsequence is "+LCS(m,n,a,b));
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
+    public static int LCS(int m,int n,String a, String b)
     {
-        // put your code here
-        return x + y;
+         int[][] dp=new int[m+1][n+1];
+         for(int i=1;i<=m;i++)
+           { for(int j=1;j<=n;j++)
+               { dp[i][j]=-1;
+                }
+            }
+          
+            int ans =LCSutil(m,n,a,b,dp);
+            return ans;
+    }
+    public static int LCSutil(int m, int n, String a, String b,int dp[][])
+    {
+        if(m==0 ||n==0)
+         return 0;
+         
+        if(dp[m][n]!=-1)
+        { return dp[m][n];
+        }
+        
+        if(a.charAt(m-1)==b.charAt(n-1))
+         return dp[m][n]=1+LCSutil(m-1,n-1,a,b,dp);
+         else
+         return dp[m][n]=Math.max(LCSutil(m-1,n,a,b,dp),LCSutil(m,n-1,a,b,dp));
     }
 }

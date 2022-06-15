4c3c7c99dd1cbe978de2aae0381f17c64bd9ebbc
KOUNIK MAITRA <kounikmaitra2633@gmail.com>
2022-06-15 17:38:15 +0530

    Update LongestCommonSubsequence_BottomUp.java

--- Dynamic-Programing/LongestCommonSubsequence_BottomUp.java
+++ Dynamic-Programing/LongestCommonSubsequence_BottomUp.java
@@ -1,33 +1,32 @@
-
-/**
- * Write a description of class LongestCommonSubsequence_BottomUp here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class LongestCommonSubsequence_BottomUp
+import java.io.*;
+import java.util.*;
+ class LongestCommonSubsequence_BottomUp
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class LongestCommonSubsequence_BottomUp
-     */
-    public LongestCommonSubsequence_BottomUp()
+   public static void main (String args[])
     {
-        // initialise instance variables
-        x = 0;
+        String a="ABCAB"; 
+        String b="AECB";
+        int m=a.length();
+        int n=b.length();
+        System.out.println("length of the longest subsequence is "+LCS(m,n,a,b));
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
+   public static int LCS(int m,int n,String a, String b)
     {
-        // put your code here
-        return x + y;
+         int[][] dp=new int[m+1][n+1];
+            int ans =LCSutil(m,n,a,b,dp);
+            return ans;
+    }
+   public static int LCSutil(int m, int n, String a, String b,int dp[][])
+    {
+       for(int i=1;i<=m;i++)
+       {for(int j=1;j<=n;j++)
+           { if(a.charAt(i-1)==b.charAt(j-1))
+               dp[i][j]=1+dp[i-1][j-1];
+               else
+               dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        
+        return dp[m][n];
     }
 }
